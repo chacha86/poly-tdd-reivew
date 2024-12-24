1f026d2e3456e5ff91e023e7da75e54091ed5309
asungkim <asungkim007@naver.com>
2024-12-24 10:47:25 +0900

    테스트 코드 추가 , Calc 클래스 전체적으로 변화 , 주석을 통해 해석 용이 및 고려 사항 추가

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,61 +1,66 @@
 public class Calc {
     public static int run(String s) {
-
-        // (3+5) * 5
         s=s.replace(" ","");
         return execute(s);
     }
+
+    /***
+     * 코드 고려 사항
+     * 1. '-' 처리 => 연산자인지 아닌지
+     * 2. 동일 우선순위 -> +,- 가 연달아 나올떄 처리 방법
+     * 3. 괄호 처리 방법
+     * 재귀니까 우선순위 낮은것부터 처리해야함 (반대흐름)
+     */
 
     private static int execute(String s) {
         if (s.startsWith("(")&&s.endsWith(")")) {
             return run(s.substring(1,s.length()-1));
         }
 
-        int idx=findLowerPriority(s);
+        int idx=findLowerPriority(s,'+','-');
         if (idx != -1) {
             char op=s.charAt(idx);
             int left=execute(s.substring(0,idx));
             int right=execute(s.substring(idx+1));
-
             return calculate(left,right,op);
         }
+
+        idx=findLowerPriority(s,'*','/');
+        if (idx != -1) {
+            char op=s.charAt(idx);
+            int left=execute(s.substring(0,idx));
+            int right=execute(s.substring(idx+1));
+            return calculate(left,right,op);
+        }
+
+        if (s.startsWith("-")) {
+            return (-1)*execute(s.substring(1));
+        }
+
 
         return Integer.parseInt(s);
     }
 
-
-    // (3+5)*5+-4
-    private static int findLowerPriority(String s) {
-        int tmp=0;
-        int min=Integer.MAX_VALUE;
-        int minIdx=-1;
-
-        for (int i = 0; i < s.length(); i++) {
+    private static int findLowerPriority(String s,char op1,char op2) {
+        int depth=0;
+        // 뒤에서 순회하는 이유는 좌->우 우선순위를 지키기 위해
+        for (int i = s.length()-1; i >= 0 ; i--) {
             char c=s.charAt(i);
-            if (c=='(') tmp++;
-            else if (c==')') tmp--;
-            else if (tmp==0&&(c=='+'||c=='-'||c=='*'||c=='/')) {
-                if (c=='-' &&(i==0||s.charAt(i-1)=='(')) {
+            if (c==')') depth++;
+            else if (c=='(') depth--;
+            else if ((depth==0) && (c==op1||c==op2)) {
+                // 연산자가 아닌 경우 (맨 앞에 있거나 c 앞에 연산자가 있는 경우)
+                if (c=='-'&&(i==0||"+-*/(".indexOf(s.charAt(i-1))!=-1)) {
                     continue;
                 }
-                int level = level(c);
-                if (level<min) {
-                    min=level;
-                    minIdx=i;
-                }
+                return i;
             }
         }
 
-
-        return minIdx;
+        return -1;
     }
 
-    private static int level(char c) {
-        if (c=='+'||c=='-') {
-            return 1;
-        }
-        else return 2;
-    }
+
 
     private static int calculate(int left, int right, char operator) {
         int an=0;
