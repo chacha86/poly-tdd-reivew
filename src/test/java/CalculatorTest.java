1975bc6ed2bb55cb616551d7ab7ac4425b31b1be
omegafrog <jiwoo22110103@gmail.com>
2025-01-03 12:43:49 +0900

    test: 테스트 케이스 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,66 +10,225 @@ public class CalculatorTest {
     @Test
     @DisplayName("덧셈")
     void runTest() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 + 2");
-        assertThat(res).isEqualTo(3.0);
+        Number res = Calculator.run("1 + 2");
+        assertThat(res).isEqualTo(3);
     }
     @Test
     @DisplayName("뺄셈")
     void runTest1() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 - 2");
-        assertThat(res).isEqualTo(-1.0);
+        Number res = Calculator.run("1 - 2");
+        assertThat(res).isEqualTo(-1);
     }
     @Test
     @DisplayName("곱셈")
     void runTest2() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 * 2");
-        assertThat(res).isEqualTo(2.0);
+        Number res = Calculator.run("1 * 2");
+        assertThat(res).isEqualTo(2);
     }@Test
     @DisplayName("나눗셈")
     void runTest3() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 / 2");
-        assertThat(res).isEqualTo(0.5);
+        Number res = Calculator.run("1 / 2");
+        assertThat(res).isEqualTo(0);
     }
 
     @Test
     @DisplayName("여러항 계산")
     void runTest4() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 + 2 + 3");
-        assertThat(res).isEqualTo(6.0);
+        Number res = Calculator.run("1 + 2 + 3");
+        assertThat(res).isEqualTo(6);
     }
 
     @Test
     @DisplayName("순서 정렬이 필요한 여러항 계산")
     void runTest5() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 + 2 * 3");
-        assertThat(res).isEqualTo(7.0);
+        Number res = Calculator.run("1 + 2 * 3");
+        assertThat(res).isEqualTo(7);
     }
 
     @Test
     @DisplayName("괄호가 포함된 여러항 계산")
     void runTest6() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 + (-4 + 2) * 3");
-        assertThat(res).isEqualTo(-5.0);
+        Number res = Calculator.run("1 + (-4 + 2) * 3");
+        assertThat(res).isEqualTo(-5);
     }
     @Test
     @DisplayName("중첩된 괄호가 포함된 여러항 계산")
     void runTest7() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("1 + (-4 + (6 + 2) * 2) * 3");
-        assertThat(res).isEqualTo(37.0);
+        Number res = Calculator.run("1 + (-4 + (6 + 2) * 2) * 3");
+        assertThat(res).isEqualTo(37);
     }
     @Test
     @DisplayName("최종 결과")
     void runTest8() {
-        Calculator calculator = new Calculator();
-        Number res = calculator.run("((3 + 5) * 5 + -10) * 10 / 5");
-        assertThat(res).isEqualTo(60.0);
+        Number res = Calculator.run("((3 + 5) * 5 + -10) * 10 / 5");
+        assertThat(res).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1() {
+        assertThat(Calculator.run("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2() {
+        assertThat(Calculator.run("2 + 1")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        assertThat(Calculator.run("2 + 2")).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("1000 + 280 == 1280")
+    void t4() {
+        assertThat(Calculator.run("1000 + 280")).isEqualTo(1280);
+    }
+
+    @Test
+    @DisplayName("2 - 1 == 1")
+    void t5() {
+        assertThat(Calculator.run("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3 - 1 == 2")
+    void t6() {
+        assertThat(Calculator.run("3 - 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")
+    void t7() {
+        assertThat(Calculator.run("100 - 20")).isEqualTo(80);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t8() {
+        assertThat(Calculator.run("10 + 20 + 30")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    void t9() {
+        assertThat(Calculator.run("10 - 20 + 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 == -20")
+    void t10() {
+        assertThat(Calculator.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+    void t11() {
+        assertThat(Calculator.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+
+    @Test
+    @DisplayName("10 * 10 == 100")
+    void t12() {
+        assertThat(Calculator.run("10 * 10")).isEqualTo(100);
+    }
+
+    @Test
+    @DisplayName("10 * -10 == -100")
+    void t13() {
+        assertThat(Calculator.run("10 * -10")).isEqualTo(-100);
+    }
+
+    @Test
+    @DisplayName("10 * 10 * 10 == 1000")
+    void t14() {
+        assertThat(Calculator.run("10 * 10 * 10")).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("10 + 5 * 2 == 20")
+    void t15() {
+        assertThat(Calculator.run("10 + 5 * 2")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("20 + 10 + 5 * 2 == 40")
+    void t16() {
+        assertThat(Calculator.run("20 + 10 + 5 * 2")).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("10 * 20 + 10 + 5 * 2 == 220")
+    void t17() {
+        assertThat(Calculator.run("10 * 20 + 10 + 5 * 2")).isEqualTo(220);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    void t18() {
+        assertThat(Calculator.run("(10 + 20)")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("((10 + 20)) == 30")
+    void t19() {
+        assertThat(Calculator.run("((10 + 20))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(((10 + 20))) == 30")
+    void t20() {
+        assertThat(Calculator.run("(((10 + 20)))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(20 + 20) + 20 == 60")
+    void t21() {
+        assertThat(Calculator.run("(20 + 20) + 20")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("((20 + 20)) + 20 == 60")
+    void t22() {
+        assertThat(Calculator.run("((20 + 20)) + 20")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    void t23() {
+        assertThat(Calculator.run("(10 + 20) * 3")).isEqualTo(90);
+    }
+
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t24() {
+        assertThat(Calculator.run("10 + (10 + 5)")).isEqualTo(25);
+    }
+
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    void t25() {
+        assertThat(Calculator.run("-(10 + 5)")).isEqualTo(-15);
+    }
+
+    @Test
+    @DisplayName("-(8 + 2) * -(7 + 3) + 5 == 105")
+    void t26() {
+        assertThat(Calculator.run("-(8 + 2) * -(7 + 3) + 5")).isEqualTo(105);
+    }
+
+    @Test
+    @DisplayName("5 - (1 + 5) == -1")
+    void t27() {
+        assertThat(Calculator.run("5 - (1 + 5)")).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("3 * 1 + (1 - (4 * 1 - (1 - 1))) == 0")
+    void t28() {
+        assertThat(Calculator.run("3 * 1 + (1 - (4 * 1 - (1 - 1)))")).isEqualTo(0);
     }
 }
